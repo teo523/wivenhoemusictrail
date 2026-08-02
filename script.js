086c956ab72e869a747916bb95dba1f931431f68
teo523 <teodoro.dan@gmail.com>
2026-08-02 19:13:31 +0100

    phone size

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 const notes=["♪","♫","♩","♬","♭"];
 const bg=document.querySelector(".background-notes");
-
 for(let i=0;i<20;i++){
- const n=document.createElement("div");
- n.className="note";
- n.textContent=notes[Math.floor(Math.random()*notes.length)];
- n.style.left=Math.random()*100+"vw";
- n.style.top=Math.random()*100+"vh";
- n.style.fontSize=(18+Math.random()*28)+"px";
- n.style.animationDuration=(10+Math.random()*12)+"s";
- n.style.animationDelay=(-Math.random()*20)+"s";
- bg.appendChild(n);
+  const note=document.createElement("div");
+  note.className="note";
+  note.textContent=notes[Math.floor(Math.random()*notes.length)];
+  note.style.left=Math.random()*100+"vw";
+  note.style.top=Math.random()*100+"vh";
+  note.style.fontSize=(18+Math.random()*28)+"px";
+  note.style.animationDuration=(10+Math.random()*12)+"s";
+  note.style.animationDelay=(-Math.random()*20)+"s";
+  bg.appendChild(note);
 }
